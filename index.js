d731d058632d072fb861e9053c4266a994249078
Hannah <hannahwoodward@hotmail.com>
2021-10-02 19:59:14 +0800

    not able to test completed

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
-// Code your solutions in this file
+
+// question 1
 for (let age = 30; age < 40; age++) {
-    console.log (`I'm ${age} years old. Happy birthday to me!`);
-  debugger;
+  console.log (`I'm ${age} years old. Happy birthday to me!`);
+}
+
+// question 2
+function writeCards (arr, event) {
+let thankYouCards = [];
+for (let i = 0; i < arr.length; i++){
+  thankYouCards.push(`Thank you ${arr[i]}, for the wonderful ${event} gift!`) 
+  }
+  return thankYouCards;
+}
+
+console.log(writeCards(["Ada", "Brendan", "Ali"], "birthday"));
+
+// A for loop that prints 1, 2, and 3
+for (let counterOne = 1; counterOne < 4; counterOne++){
+console.log(counterOne);
+}
+
+// A while loop that prints 1, 2, and 3
+let counterTwo = 1;
+while (counterTwo < 4) {
+console.log(counterTwo);
+counterTwo++;
 }
